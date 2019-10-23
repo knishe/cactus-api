17c6a6676069bba45916971e0c30a74497cd78fb
Nishanthan Krishnakumar <knishe@gmail.com>
2019-10-24 01:22:33 +0800

    Added new vehicle api with graphql

---
+++ src/main/java/com/aegle/cactus/CactusApi.java
@@ -0,0 +1,11 @@
+package com.aegle.cactus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CactusApi {
+    public static void main(String[] args) {
+        SpringApplication.run(CactusApi.class,args);
+    }
+}
